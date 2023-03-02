a96f31d0d258cdd68009d9c731072fd04381ba46
Andy Fuchs <andy@foxwerk.de>
2023-03-02 12:20:02 +0100

    adding tabs

--- src/com.elgato.template.sdPlugin/actions/template/property-inspector/inspector.js
+++ src/com.elgato.template.sdPlugin/actions/template/property-inspector/inspector.js
@@ -18,6 +18,10 @@ $PI.onConnected((jsn) => {
     );
 });
 
+$PI.onDidReceiveGlobalSettings(({payload}) => {
+    console.log('onDidReceiveGlobalSettings', payload);
+})
+
 /**
  * Provide window level functions to use in the external window
  * (this can be removed if the external window is not used)
@@ -29,3 +33,64 @@ window.sendToInspector = (data) => {
 document.querySelector('#open-external').addEventListener('click', () => {
     window.open('../../../external.html');
 });
+
+
+/** 
+ * TABS
+ * ----
+ * 
+ * This will make the tabs interactive:
+ * - clicking on a tab will make it active
+ * - clicking on a tab will show the corresponding content
+ * - clicking on a tab will hide the content of all other tabs
+ * - a tab must have the class "tab"
+ * - a tab must have a data-target attribute that points to the id of the content
+ * - the content must have the class "tab-content"
+ * - the content must have an id that matches the data-target attribute of the tab
+ * - the content must have a data-tab attribute that matches the data-target attribute of the tab
+ *  <div class="tab selected" data-target="#tab1" title="Show some inputs">Inputs</div>
+ *  <div class="tab" data-target="#tab2" title="Here's some text-areas">Text</div>
+ * a complete tab-example can be found in the index.html
+   <div type="tabs" class="sdpi-item">
+      <div class="sdpi-item-label empty"></div>
+      <div class="tabs">
+        <div class="tab selected" data-target="#tab1" title="Show some inputs">Inputs</div>
+        <div class="tab" data-target="#tab2" title="Here's some text-areas">Text</div>
+      </div>
+    </div>
+    <hr class="tab-separator" />
+ * You can use the code below to activate the tabs (`activateTabs` and `clickTab` are required)
+ */
+
+function activateTabs(activeTab) {
+    const allTabs = Array.from(document.querySelectorAll('.tab'));
+    let activeTabEl = null;
+    allTabs.forEach((el, i) => {
+        el.onclick = () => clickTab(el);
+        if(el.dataset?.target === activeTab) {
+            activeTabEl = el;
+        }
+    });
+    if(activeTabEl) {
+        clickTab(activeTabEl);
+    } else if(allTabs.length) {
+        clickTab(allTabs[0]);
+    }
+}
+
+function clickTab(clickedTab) {
+    const allTabs = Array.from(document.querySelectorAll('.tab'));
+    allTabs.forEach((el, i) => el.classList.remove('selected'));
+    clickedTab.classList.add('selected');
+    activeTab = clickedTab.dataset?.target;
+    allTabs.forEach((el, i) => {
+        if(el.dataset.target) {
+            const t = document.querySelector(el.dataset.target);
+            if(t) {
+                t.style.display = el == clickedTab ? 'block' : 'none';
+            }
+        }
+    });
+}
+
+activateTabs();
